b4029236d5b54d47ad1152fa25b633fc083c0c96
JuniorLauro <lauro.jr199@gmail.com>
2022-12-11 10:38:20 -0300

    add logs in console

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 
 class ClassComponent extends React.Component {
+    constructor(props: any){
+        super(props)
+        console.log('constructor reached')
+    }
+    
     state= {
         name: 'Mundo'
     }
 
+    componentDidMount() {
+        console.log('did mount reached')
+    }
+
+    componentDidUpdate() {
+        console.log('did update reached')
+    }
+
     render() {
+        console.log('render reached')
         return  <div>
             <p>name: {this.state.name}</p>
             <button onClick={() =>
                 this.setState({name: 'Pessoa'})
-            }>CLique aqui!</button>
+            }>Clique aqui!</button>
         </div>
     }
 }
