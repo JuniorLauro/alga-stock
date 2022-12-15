301df083fc431d2caae120bcd5485696fb73f976
JuniorLauro <lauro.jr199@gmail.com>
2022-12-15 20:59:47 -0300

    add button componet

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import Button from '../Button';
 import Header from '../Header/index';
 import './App.css';
+
+function TestComponent(){
+  return <img width='15' src='https://cdn-icons-png.flaticon.com/512/64/64673.png'
+  alt='Cubo Mágico'/>
+}
 
 function App() {
   return (
     <div className="App">
       <Header title='AlgaStock'/> 
+
+      <div className="Container">
+        <Button
+          onCLick={() => window.alert('teste de alerta')}
+          appendIcon={<TestComponent />}
+        >
+          Alert
+        </Button>
+      </div>
     </div>
   );
 }
